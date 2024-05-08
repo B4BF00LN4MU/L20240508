b8701b523921d70e495b1c2ceb0346547c6d878e
B4BF00LN4MU <boy566@naver.com>
2024-05-08 16:24:50 +0900

    strcmp

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 int main()
 {
-	char greeting[100] = "Hello,";
-	char name[100];
+	char s1[] = "sample";
+	char s2[] = "simple";
 
-	scanf("%s", name);
-
-	strcat(greeting, name);
-	strcat(greeting, "!");
-
-	printf("%s", greeting);
+	if (strcmp(s1, s2) < 0)
+		printf("%s는 %s보다 사전순으로 빠릅니다.\n", s1, s2);
+	else if (strcmp(s1, s2) == 0)
+		printf("%s는 %s과 일치합니다.\n", s1, s2);
+	else
+		printf("%s는 %s보다 사전순으로 늦습니다.\n", s1, s2);
 
 	return 0;
 }
